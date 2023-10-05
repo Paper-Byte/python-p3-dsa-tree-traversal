0198215c5f435507a1aec14321f269540fdf4e10
Noah <noahhughesph@gmail.com>
2023-10-04 21:48:21 -0400

    all tests passing

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,23 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    # breadth-first answer
+    def get_element_by_id(self, id):
+        nodes_to_visit = [self.root]
+        while len(nodes_to_visit) > 0:
+            node = nodes_to_visit.pop(0)
+            if (node['id'] == id):
+                return node
+            nodes_to_visit += node['children']
+        return None
+
+    # depth-first search
+    # def get_element_by_id(self, id):
+    #     nodes_to_visit = [self.root]
+    #     while len(nodes_to_visit) > 0:
+    #         node = nodes_to_visit.pop(0)
+    #         if (node['id'] == id):
+    #             return node
+    #         nodes_to_visit = node['children'] + nodes_to_visit
+    #     return None
