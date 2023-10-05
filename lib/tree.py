6a5620de313865858094a5307c64a1e5936f3af7
Noah <noahhughesph@gmail.com>
2023-10-04 22:05:46 -0400

    all tests passing, depth first and depth first recursively also done

--- lib/tree.py
+++ lib/tree.py
@@ -12,12 +12,26 @@ class Tree:
             nodes_to_visit += node['children']
         return None
 
-    # depth-first search
+    # depth-first answer
     # def get_element_by_id(self, id):
     #     nodes_to_visit = [self.root]
     #     while len(nodes_to_visit) > 0:
     #         node = nodes_to_visit.pop(0)
     #         if (node['id'] == id):
     #             return node
-    #         nodes_to_visit = node['children'] + nodes_to_visit
+    #         nodes_to_visit += node['children']
+    #     return None
+
+    # depth-first answer recursively
+    # def get_element_by_id(self, id, node=None):
+    #     if (node is None):
+    #         node = self.root
+
+    #     if (node['id'] == id):
+    #         return node
+
+    #     for child in node['children']:
+    #         result = self.get_element_by_id(id, child)
+    #         if result:
+    #             return result
     #     return None
